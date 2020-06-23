da53546f2ef9da58184b30348dc972c5be5b7cb0
yujeongJeon <workingnewjeong@gmail.com>
2020-06-23 17:31:01 +0900

    (2020-06-23) updated by juj: update routing module

---
+++ src/app/modules/menu/components/pw-detail/pw-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-pw-detail',
+  templateUrl: './pw-detail.component.html',
+  styleUrls: ['./pw-detail.component.scss']
+})
+export class PwDetailComponent {
+  
+}
